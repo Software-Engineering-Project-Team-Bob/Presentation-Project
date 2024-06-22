0d94642d35f04b829ed3a8d482c515269a877121
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2024-06-22 20:46:47 +0530

    Adding the presentation of the Software Engineering Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div style={{ width: "100vw", height: "100vh" }}>
+			<iframe
+				title="ruleBook"
+				src="/Online Class Management System.pdf"
+				width="100%"
+				height="100%"
+			></iframe>
+		</div>
+	);
+}
+
+export default App;
